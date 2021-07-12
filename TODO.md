855ff4cff74620ba56aaf40bb1178aa8f2281d61
fabiitch <fabien.occelli@hotmail.fr>
2021-07-12 02:29:50 +0200

    rdy

---
+++ TODO.md
@@ -0,0 +1,1 @@
+* Use custom pools for event
--- src/test/java/com/nzt/b2d/test/NztB2dTestStarter.java
+++ src/test/java/com/nzt/b2d/test/NztB2dTestStarter.java
@@ -1,26 +1,19 @@
 package com.nzt.b2d.test;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nzt.gdx.main.AbstractMain;
-import com.nzt.gdx.test.trials.NztTestListStarter;
-import com.nzt.gdx.test.trials.tester.selector.ScreenSelectorTestMain;
+import com.nzt.b2d.test.screens.STDebugDisplaySystemTest;
+import com.nzt.gdx.test.trials.tester.archi.main.FastTesterMain;
+import com.nzt.gdx.test.trials.tester.archi.main.StarterTestConfig;
 
+/**
+ * Use it for Test one class with screenTestClass
+ */
 public class NztB2dTestStarter {
-
-    public static void startLwjgl3(AbstractMain main, int witdh, int height) {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Tester Lwjgl3");
-        configuration.setWindowedMode(witdh, height);
-        configuration.setWindowIcon("box2d.png");
-        new Lwjgl3Application(main, configuration);
-    }
+    private static final Class screenTestClass = STDebugDisplaySystemTest.class;
 
     private static final int witdh = 800;
     private static final int height = 500;
 
     public static void main(String[] args) {
-        startLwjgl3(new ScreenSelectorTestMain(NztTestListStarter.class, "com.nzt.b2d.test"),
-                witdh, height);
+        NztB2dTestListStarter.startLwjgl3(new FastTesterMain(screenTestClass), witdh, height);
     }
 }
