6b0199479201d6bf5d565fd8cc1b25e43b8852e1
Fabiitch <fabien.occelli@hotmail.fr>
2021-12-10 12:18:43 +0100

    fix from nzt

--- src/main/java/com/nzt/b2d/debug/B2dHudDebug.java
+++ src/main/java/com/nzt/b2d/debug/B2dHudDebug.java
@@ -13,12 +13,12 @@ public class B2dHudDebug {
     }
 
     public B2dHudDebug(World world, int positionOnStage, Color color) {
-        HudDebug.addItem("Body count", world.getBodyCount(), positionOnStage, color);
-        HudDebug.addItem("Contact count", world.getContactCount(), positionOnStage, color);
-        HudDebug.addItem("Fixture count", world.getFixtureCount(), positionOnStage, color);
-        HudDebug.addItem("Joint count", world.getJointCount(), positionOnStage, color);
-        HudDebug.addItem("Proxy count", world.getProxyCount(), positionOnStage, color);
-        HudDebug.addItem("World VelocityThreshold", World.getVelocityThreshold(), positionOnStage, color);
+        HudDebug.add("Body count", world.getBodyCount(), positionOnStage, color);
+        HudDebug.add("Contact count", world.getContactCount(), positionOnStage, color);
+        HudDebug.add("Fixture count", world.getFixtureCount(), positionOnStage, color);
+        HudDebug.add("Joint count", world.getJointCount(), positionOnStage, color);
+        HudDebug.add("Proxy count", world.getProxyCount(), positionOnStage, color);
+        HudDebug.add("World VelocityThreshold", World.getVelocityThreshold(), positionOnStage, color);
     }
 
     public void remove() {
